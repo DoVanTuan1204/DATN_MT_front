1f7293d87eeb6ca564644f21d494d6db2a13092b
DoVanTuan1204 <dovantuan1404@gmail.com>
2023-05-12 17:08:54 +0700

    add style for loginpage

---
+++ pages/thanh-toan-thanh-cong.jsx
@@ -0,0 +1,36 @@
+import IconSuccess from "@/src/components/icon/IconSuccess";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+
+const PaymentSuccess = () => {
+  const router = useRouter();
+  const [counter, setCounter] = useState(5);
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    if (counter === 0) {
+      router.push("/");
+    }
+  }, [counter]);
+  const backToFirstPage = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="h-[100vh] flex justify-center items-center">
+      <div className="flex flex-col items-center gap-5">
+        <div className="text-green-500 w-40">
+          <IconSuccess />
+        </div>
+        <span className="text-3xl text-orange-400">Thanh toán thành công</span>
+        <span className="text-xl">Bạn sẽ quay về trang chủ sau {counter}</span>
+        <button
+          onClick={backToFirstPage}
+          className="p-4 bg-orange-500 rounded-lg text-xl text-white">
+          Tiếp tục mua sắm
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
